a78d1603d0b1a7e018b198d18d0733feec18fce0
Rajcody <raj.laskar2016@gmail.com>
2021-05-29 01:59:45 +0530

    added checkout page

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAQvSIDJH95FblLxrDzdon_9YxR06rdnOA",
+    authDomain: "amaz2-0.firebaseapp.com",
+    projectId: "amaz2-0",
+    storageBucket: "amaz2-0.appspot.com",
+    messagingSenderId: "742931764279",
+    appId: "1:742931764279:web:fa3d74b9e16f96698e5bb2",
+    measurementId: "G-HENDR96XVV"
+  };
--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     images :{
-        domains :['1079life.com', 'fakestoreapi.com']
+        domains :['1079life.com','links.papareact.com', 'fakestoreapi.com']
     },
 };
